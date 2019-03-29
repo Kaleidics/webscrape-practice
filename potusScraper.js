ed95e6cfe26ff194e0f0d61a7da38e6868123cf0
Eddie Chu <contact@eddiechu.me>
2019-03-28 22:59:56 -0700

    debugging scraper promise

--- potusScraper.js
+++ potusScraper.js
@@ -1,12 +1,23 @@
 'use strict';
 
-const rp = require('require-promise');
+const rp = require('request-promise');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
-rp(url)
-    .then(html => {
-        console.log(html);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+const cheerio = {
+    scraper: function () {
+        rp(url)
+            .then(html => {
+                //fetches data, should be a string, confirmed by console log
+                console.log('scraper is returning data as:', typeof html);
+                //return the string to use in server.js '/' route handler, currently getting undefined
+                console.log(html.slice(0, 500));
+                //it does return html with html tags, head tags and etc...
+                return(html);
+            })
+            .catch(err => {
+                return(err);
+        });
+    }
+}
+
+module.exports = cheerio;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cheerio = require('./potusScraper');
 
 app.use(express.json());
 app.use(
@@ -11,7 +12,14 @@ app.use(
     })
 );
 
-
+app.get('/', (req, res) => {
+    //using scraper method from potusScraper.js
+    // cheerio.scraper()
+    //     .then(text => { //failing here. Doesn't look like cheerio.scraper is returning a value
+    //     console.log(text);
+    // });
+    console.log(cheerio.scraper());
+});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);

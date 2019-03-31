01c846bce58e4d12c8cf658b996f8acd3fd039c0
Eddie Chu <contact@eddiechu.me>
2019-03-31 01:06:41 -0700

    debugging scraper

--- potusScraper.js
+++ potusScraper.js
@@ -27,9 +27,10 @@ const cheerio = {
                 return browser.newPage();
             })
             .then(page => {
-                return page.goto(url2)
+                return page.goto(url2, { waitUntil: 'networkidle0' })
                     .then(() => {
-                        page.evaluate(_ => {
+                        //attempting to scroll through page to cause loading in more elements, doesn't seem to work
+                        page.evaluate(() => {
                             window.scrollBy(0, window.innerHeight);
                         })
                         return page.content()
@@ -43,12 +44,14 @@ const cheerio = {
                 const numLinks = $('.scrollerItem div:nth-of-type(2) article div div:nth-of-type(3) a', html).attr('href').length;
                 const links = $('.scrollerItem div:nth-of-type(2) article div div:nth-of-type(3) a', html);
 
-                for (let i=0; i<numLinks; i++) {
-                    urls.push(links[i]);
+                //was using numLinks as increment counter but was getting undefined, as the pages only seems to load inconsistent between 10-20 elements
+                for (let i=0; i<24; i++) {
+                    urls.push(links[i].attribs.href);
                 }
                 
-                console.log('start of', urls);
-                console.log(urls.length)
+                console.log('start of urls:', urls);
+                console.log('scraped urls:', urls.length);
+                console.log('numLinks:', numLinks);
                 // console.log($('.scrollerItem div:nth-of-type(2) article div div:nth-of-type(3) a', html).length);
             })
             .catch(err => console.log(err));
